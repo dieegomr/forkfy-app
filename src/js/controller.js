968236e2bb58da9ae505e83df9f170d2f31af5a3
dieegomr <dmendes.rocha@gmail.com>
2022-07-13 14:39:36 -0300

    DOM updating algorithm implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search results
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) Loading the recipe
     await model.loadRecipe(id);
 
@@ -64,7 +67,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
